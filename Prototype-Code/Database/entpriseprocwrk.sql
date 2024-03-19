2b08568c1db3305befae83ed7261fe89c2aff270
MohamedAwad22005293 <160639977+MohamedAwad22005293@users.noreply.github.com>
2024-03-19 01:13:39 +0000

    Added information to user, vendors and products tables

--- Prototype-Code/Database/entpriseprocwrk.sql
+++ Prototype-Code/Database/entpriseprocwrk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2024 at 11:07 PM
+-- Generation Time: Mar 19, 2024 at 01:33 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -73,16 +73,29 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `products` (
-  `productName` varchar(255) DEFAULT NULL,
+  `productName` varchar(255) NOT NULL,
   `productCode` varchar(50) DEFAULT NULL,
   `skuCode` varchar(50) DEFAULT NULL,
   `productImage` blob DEFAULT NULL,
   `productDescription` text DEFAULT NULL,
   `productQuantity` int(11) DEFAULT NULL,
-  `price` decimal(10,2) DEFAULT NULL,
+  `price` mediumtext DEFAULT NULL,
   `bestBeforeDate` date DEFAULT NULL,
   `orderStatus` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`productName`, `productCode`, `skuCode`, `productImage`, `productDescription`, `productQuantity`, `price`, `bestBeforeDate`, `orderStatus`) VALUES
+('Basmati Rice', 'RIC002', 'SKU004', NULL, 'Aromatic and long-grain Basmati rice, 10 lb', 60, '£8.49', '2024-04-29', 'In Stock'),
+('Eggs (Large)', 'EGG001', 'SKU005', NULL, 'Farm-fresh large eggs, pack of 12', 50, '£3.79', '2024-04-23', 'In Stock'),
+('Ground Beef', 'BEEF001', 'SKU006', NULL, 'Premium quality ground beef, 1 lb', 40, '£6.99', '2024-04-26', 'In Stock'),
+('Orange Juice', 'JUC001', 'SKU007', NULL, 'Freshly squeezed orange juice, 64 oz', 70, '£4.29', '2024-03-25', 'In Stock'),
+('Sliced Bread', 'BRD001', 'SKU001', NULL, 'Freshly baked sliced white bread', 100, '£2.49', '2024-04-20', 'In Stock'),
+('White Rice', 'RIC001', 'SKU003', NULL, 'Premium quality long-grain white rice, 5 lb', 80, '£5.99', '2024-04-25', 'In Stock'),
+('Whole Wheat Bread', 'BRD002', 'SKU002', NULL, 'Nutritious whole wheat bread made with whole grain flour', 120, '£2.99', '2024-04-21', 'In Stock');
 
 -- --------------------------------------------------------
 
@@ -105,13 +118,20 @@ CREATE TABLE `sales` (
 --
 
 CREATE TABLE `users` (
-  `fullName` varchar(255) DEFAULT NULL,
+  `fullName` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `phoneNumber` varchar(20) DEFAULT NULL,
   `role` enum('Manager','Employee','Vendor') DEFAULT 'Employee',
   `vendorID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`fullName`, `email`, `password`, `phoneNumber`, `role`, `vendorID`) VALUES
+('Edwards Jackson', 'EdwardsJackson@Gmail.com', '$2y$10$5UeOY8Tw1NXWIDVcPcB/duNq2SuIcIbN97juF85uoMnAPkxdGIxqC', '07263527338', 'Manager', NULL);
 
 -- --------------------------------------------------------
 
@@ -143,6 +163,15 @@ CREATE TABLE `vendors` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `vendors`
+--
+
+INSERT INTO `vendors` (`vendorID`, `vendorName`, `vendorEmail`, `vendorPhone`, `vendorAddress`) VALUES
+(1, 'Tesco Express', 'TescoExpressManningham@Gmail.com', '0345 026 9840', 'Manningham Ln, Bradford BD8 7HY'),
+(2, 'Lidl ', 'LidlBarkerend@Gmail.com', ' 020 3966 5566', ' Barkerend Rd, Bradford BD3 9BN'),
+(3, 'Morrisons', 'MorrisonsVictoria@Gmail.com', ' 01274 498218', 'Victoria Shopping Centre, Young St, Bradford BD8 9BN');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -159,6 +188,12 @@ ALTER TABLE `orders`
   ADD PRIMARY KEY (`orderID`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productName`);
+
+--
 -- Indexes for table `sales`
 --
 ALTER TABLE `sales`
@@ -168,6 +203,7 @@ ALTER TABLE `sales`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
+  ADD PRIMARY KEY (`fullName`),
   ADD KEY `vendorID` (`vendorID`);
 
 --
@@ -209,7 +245,7 @@ ALTER TABLE `vendororders`
 -- AUTO_INCREMENT for table `vendors`
 --
 ALTER TABLE `vendors`
-  MODIFY `vendorID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `vendorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
